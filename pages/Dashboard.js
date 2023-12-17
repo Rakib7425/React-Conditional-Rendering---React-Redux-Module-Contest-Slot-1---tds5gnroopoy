f0d20f407afbd7b7d15ef2ab7eaf96bb34a05ebf
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-12-17 11:28:54 +0530

    Update Dashboard.js

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,19 +1,32 @@
-import { useContext, useEffect } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "./_app";
 
-
 function Dashboard() {
-  const { } = useContext(AuthContext);
+  const { btn1State, setBtn1State, btn2State, setBtn2State } = useContext(AuthContext);
+
+  const handleBtn1Click = () => {
+    setBtn1State(!btn1State);
+  };
+
+  const handleBtn2Click = () => {
+    setBtn2State(!btn2State);
+  };
 
   return (
     <div id="dashboardPage">
-      <button id="btn1" >Button 1</button>
-      <button id="btn2" >Button 2</button>
-      <div className="msg">Both are true</div>
-      <div className="msg">One on them is false</div>
-      <div className="msg">Both are false</div>
-
+      <button id="btn1" onClick={handleBtn1Click}>
+        Button 1
+      </button>
+      <button id="btn2" onClick={handleBtn2Click}>
+        Button 2
+      </button>
+      {btn1State && btn2State && <div className="msg">Both are true</div>}
+      {(btn1State && !btn2State) || (!btn1State && btn2State) ? (
+        <div className="msg">One of them is false</div>
+      ) : null}
+      {!btn1State && !btn2State && <div className="msg">Both are false</div>}
     </div>
   );
 }
+
 export default Dashboard;
