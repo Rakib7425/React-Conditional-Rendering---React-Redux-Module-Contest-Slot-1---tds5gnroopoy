d962faeb98b454ce7c9f0a3c6fe0cb2b82613b68
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-12-17 11:28:26 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
+// _app.js
+
 import "@/styles/globals.css";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 export const AuthContext = React.createContext();
 
 export default function App({ Component, pageProps }) {
+  const [btn1State, setBtn1State] = useState(true);
+  const [btn2State, setBtn2State] = useState(true);
 
   const contextValue = {
-
+    btn1State,
+    setBtn1State,
+    btn2State,
+    setBtn2State,
   };
 
   return (
